9c9bb722272789a149401dce22b1f3cd5abf7361
Krishna <appilivamsikrishna@gmail.com>
2024-02-22 11:25:36 +0530

    fix

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -1,22 +1,28 @@
 import React, { useState } from 'react';
+import Box from '@mui/material/Box';
 import axios from 'axios';
-import { FormControl, InputLabel, Select, MenuItem, Button, Typography } from '@mui/material';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
 
-const MyComponent = () => {
-  const [selectedOptions, setSelectedOptions] = useState({
-    option1: '',
-    option2: '',
-    option3: '',
-    option4: '',
-    option5: ''
-  });
-  const [responseData, setResponseData] = useState(null);
+const AstroForm = () => {
+  const [ayanamsa, setAyanamsa] = useState('');
+  const [coordinates, setCoordinates] = useState('');
+  const [datetime, setDateTime] = useState('');
+  const [chartType, setChartType] = useState('');
+  const [chartStyle, setChartStyle] = useState('');
+  const [format, setFormat] = useState('');
+  const [la, setLa] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
 
-  const handleOptionChange = (e, optionName) => {
-    setSelectedOptions({
-      ...selectedOptions,
-      [optionName]: e.target.value
-    });
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
   };
 
   const handleSubmit = async () => {
@@ -27,71 +33,60 @@ const MyComponent = () => {
       `https://api.prokerala.com/v2/astrology/chart?ayanamsa=1&coordinates=10.214747,78.097626&datetime=2022-03-17T10:50:40Z&chart_type=rasi&chart_style=south-indian&format=svg&la=en`,
       {
         headers: {
-          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5ZjJhMmE3Zi1hMDg5LTQxZTQtOTVmNC01OTAwOGI0MjE4YzAiLCJqdGkiOiI0YTY5NDUzZDliMjYxMTIyN2Y5Nzc5ZTU2NDA0ZWZiMzk0NmI2NmRmY2ExYmJkM2FjNWYyMzkzNDM5ODkxOGNkMTM3MDVhNGUzNzliMGNkZSIsImlhdCI6MTcwODU3OTA0OS41NzUxMTIsIm5iZiI6MTcwODU3OTA0OS41NzUxMTQsImV4cCI6MTcwODU4MjY0OS41NzQ5OTksInN1YiI6IjdmODdlOWQ4LTdmOGQtNDJhNC05ZGFmLTE1NGMzODE3ZjlkZCIsInNjb3BlcyI6W10sImNyZWRpdHNfcmVtYWluaW5nIjo1MDAwLCJyYXRlX2xpbWl0cyI6W3sicmF0ZSI6NSwiaW50ZXJ2YWwiOjYwfV19.mXKhzAZ1nXxiUQAopV7DSmmA3YKSwvoDj1IZ5vpKFOQkDHbnIoKw7huyqrC61ZYcTImOxqJxUqvCehfG4ugL315MZnvcaNDBoNLmHPt0AkH6AlPGtPK1mHEOlLfSCW4ryh8N7Tu4pBL0VeKvaUm42FGCgpopHqQLx6OnZkzy3uvXAw6jDdUwxr9nYqTSMCQDqMva1uafw61ddjMhJ3W45pIuwEsj2ELL09pe9ZtU-x5pF86gJg5CtMP02HZCs8JBQlJytRXBuV4EeIwkWlcj_1oKdEw3gVgDR1lZs0NZ72q-HpFcLuonVJ5YHiFXMemmXXRXatblAhR6hFkzAvEXFA'
+          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJiZjM5MmEwNC1mMThiLTRlMmEtOTg4OS02OGE1MjNjZjY3MDQiLCJqdGkiOiJkYjEzMWYzMzQzMTNhN2U4ZjU2NWZlMGQ4N2NlM2Y4NmUxMWM1ZmJjMGI3ZDRmZmRkNzNmNzVjZGZjZDJkYmI3MDliZTZiNzM0NzNhOWRhZSIsImlhdCI6MTcwODU4MTMwMi4wNzgxMDIsIm5iZiI6MTcwODU4MTMwMi4wNzgxMDMsImV4cCI6MTcwODU4NDkwMi4wNzc5ODYsInN1YiI6IjdmODdlOWQ4LTdmOGQtNDJhNC05ZGFmLTE1NGMzODE3ZjlkZCIsInNjb3BlcyI6W10sImNyZWRpdHNfcmVtYWluaW5nIjo1MDAwLCJyYXRlX2xpbWl0cyI6W3sicmF0ZSI6NSwiaW50ZXJ2YWwiOjYwfV19.fedylLkoRAqXChlM7fJXeu0LCf0WY9u6D6kS64ohfFofmASfFzRqMHSc43mSG87vhVWP10dumylVou8RSB3_804QPjWjiklSIJe1BnK6ezvLX6axUQ7eG_f_b32KfqK3xmqF2i7q8HTI4D8Pwu_9_ULJgkTmkXaXOXD7weoByU5oqj3-nNQxDfPNnbKPOAiGy2_cMJ9nUC_1agSNTVZATS-vmAP5NdyOTkpkl8NfUr1tUHUmX0S_i-z1JrpMdJV6pKRDVb3_O9yL-co3vTNMCzxK6c1KCA7X20g7YbcuV_L8Jj6tmznnbARx2v8JLqOXfif6MFKl8uTyXam9M6zHTw'
         }
       }
     );
-    setResponseData(response.data);
+    setResult(response.data);
 
     } catch (error) {
       console.error('Error fetching data:', error);
     }
+
+    setIsLoading(false);
   };
 
   return (
     <div>
-      <Typography variant="h4" gutterBottom>Generate your Kundali</Typography>
-      <FormControl fullWidth sx={{ marginBottom: 2 }}>
-        <InputLabel>Option 1</InputLabel>
-        <Select value={selectedOptions.option1} onChange={(e) => handleOptionChange(e, 'option1')}>
-          <MenuItem value="Option 1">Option 1</MenuItem>
-          <MenuItem value="Option 2">Option 2</MenuItem>
-          <MenuItem value="Option 3">Option 3</MenuItem>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Ayanamsa:
+          <input
+            type="text"
+            value={ayanamsa}
+            onChange={(e) => setAyanamsa(e.target.value)}
+          />
+        </label>
+        {/* Add other input fields similarly */}
+        <button type="submit" disabled={isLoading}>
+          {isLoading ? 'Loading...' : 'Submit'}
+        </button>
+      </form>
+      {error && <div>Error: {error}</div>}
+      {result && <div>Result: {JSON.stringify(result)}</div>}
+
+      <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
         </Select>
       </FormControl>
-      <FormControl fullWidth sx={{ marginBottom: 2 }}>
-        <InputLabel>Option 2</InputLabel>
-        <Select value={selectedOptions.option2} onChange={(e) => handleOptionChange(e, 'option2')}>
-          <MenuItem value="Option 1">Option 1</MenuItem>
-          <MenuItem value="Option 2">Option 2</MenuItem>
-          <MenuItem value="Option 3">Option 3</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl fullWidth sx={{ marginBottom: 2 }}>
-        <InputLabel>Option 3</InputLabel>
-        <Select value={selectedOptions.option3} onChange={(e) => handleOptionChange(e, 'option3')}>
-          <MenuItem value="Option 1">Option 1</MenuItem>
-          <MenuItem value="Option 2">Option 2</MenuItem>
-          <MenuItem value="Option 3">Option 3</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl fullWidth sx={{ marginBottom: 2 }}>
-        <InputLabel>Option 4</InputLabel>
-        <Select value={selectedOptions.option4} onChange={(e) => handleOptionChange(e, 'option4')}>
-          <MenuItem value="Option 1">Option 1</MenuItem>
-          <MenuItem value="Option 2">Option 2</MenuItem>
-          <MenuItem value="Option 3">Option 3</MenuItem>
-        </Select>
-      </FormControl>
-      <FormControl fullWidth sx={{ marginBottom: 2 }}>
-        <InputLabel>Option 5</InputLabel>
-        <Select value={selectedOptions.option5} onChange={(e) => handleOptionChange(e, 'option5')}>
-          <MenuItem value="Option 1">Option 1</MenuItem>
-          <MenuItem value="Option 2">Option 2</MenuItem>
-          <MenuItem value="Option 3">Option 3</MenuItem>
-        </Select>
-      </FormControl>
-      <Button variant="contained" color="primary" onClick={handleSubmit} disabled={!selectedOptions.option1 || !selectedOptions.option2 || !selectedOptions.option3 || !selectedOptions.option4 || !selectedOptions.option5}>
-        Submit
-      </Button>
-      {responseData && (
-        <div>
-          <Typography variant="h5" gutterBottom>Response Data:</Typography>
-          <pre>{JSON.stringify(responseData, null, 2)}</pre>
-        </div>
-      )}
+
+      <Button variant="contained">submit</Button>
+    </Box>
     </div>
+
+    
   );
 };
 
-export default MyComponent;
+export default AstroForm;
