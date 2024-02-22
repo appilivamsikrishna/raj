89d59ffc4284240d472f1f528164339e55ec96ca
Krishna <appilivamsikrishna@gmail.com>
2024-02-22 11:30:27 +0530

    testing

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -17,8 +17,9 @@ const AstroForm = () => {
   const [la, setLa] = useState('');
   const [isLoading, setIsLoading] = useState(false);
   const [result, setResult] = useState(null);
+  const [response, setResponse] = useState(null);
   const [error, setError] = useState(null);
-
+  const [loading, setLoading] = useState(false);
   const [age, setAge] = React.useState('');
 
   const handleChange = (event) => {
@@ -44,6 +45,30 @@ const AstroForm = () => {
     }
 
     setIsLoading(false);
+  };
+
+  const handleClick = async () => {
+    setLoading(true);
+    setError(null);
+    setResult(null);
+    try {
+     // const response = await axios.get(`https://api.example.com/data/${selectedOptions.option1}/${selectedOptions.option2}/${selectedOptions.option3}/${selectedOptions.option4}/${selectedOptions.option5}`);
+     // setResponseData(response.data);
+     const response = await axios.get(
+      `https://api.prokerala.com/v2/astrology/chart?ayanamsa=1&coordinates=10.214747,78.097626&datetime=2022-03-17T10:50:40Z&chart_type=rasi&chart_style=south-indian&format=svg&la=en`,
+      {
+        headers: {
+          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJiZjM5MmEwNC1mMThiLTRlMmEtOTg4OS02OGE1MjNjZjY3MDQiLCJqdGkiOiJkYjEzMWYzMzQzMTNhN2U4ZjU2NWZlMGQ4N2NlM2Y4NmUxMWM1ZmJjMGI3ZDRmZmRkNzNmNzVjZGZjZDJkYmI3MDliZTZiNzM0NzNhOWRhZSIsImlhdCI6MTcwODU4MTMwMi4wNzgxMDIsIm5iZiI6MTcwODU4MTMwMi4wNzgxMDMsImV4cCI6MTcwODU4NDkwMi4wNzc5ODYsInN1YiI6IjdmODdlOWQ4LTdmOGQtNDJhNC05ZGFmLTE1NGMzODE3ZjlkZCIsInNjb3BlcyI6W10sImNyZWRpdHNfcmVtYWluaW5nIjo1MDAwLCJyYXRlX2xpbWl0cyI6W3sicmF0ZSI6NSwiaW50ZXJ2YWwiOjYwfV19.fedylLkoRAqXChlM7fJXeu0LCf0WY9u6D6kS64ohfFofmASfFzRqMHSc43mSG87vhVWP10dumylVou8RSB3_804QPjWjiklSIJe1BnK6ezvLX6axUQ7eG_f_b32KfqK3xmqF2i7q8HTI4D8Pwu_9_ULJgkTmkXaXOXD7weoByU5oqj3-nNQxDfPNnbKPOAiGy2_cMJ9nUC_1agSNTVZATS-vmAP5NdyOTkpkl8NfUr1tUHUmX0S_i-z1JrpMdJV6pKRDVb3_O9yL-co3vTNMCzxK6c1KCA7X20g7YbcuV_L8Jj6tmznnbARx2v8JLqOXfif6MFKl8uTyXam9M6zHTw'
+        }
+      }
+    );
+    setResponse(response.data);
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+
+    setLoading(false);
   };
 
   return (
@@ -81,7 +106,15 @@ const AstroForm = () => {
         </Select>
       </FormControl>
 
-      <Button variant="contained">submit</Button>
+      <Button
+        variant="contained"
+        onClick={handleClick}
+        disabled={loading}
+      >
+        {loading ? 'Loading...' : 'Submit'}
+      </Button>
+      {error && <div>Error: {error}</div>}
+      {result && <div>Result: {JSON.stringify(response)}</div>}
     </Box>
     </div>
 
