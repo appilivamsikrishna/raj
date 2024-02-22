5a27de597213289d58e4854400f6c35acdc61e85
Appili Vamsi Krishna <appilivamsikrishna@gmail.com>
2024-02-22 18:33:45 +0530

    fix check

--- src/components/TabComponent.js
+++ src/components/TabComponent.js
@@ -55,14 +55,28 @@ const AstroForm = () => {
       // const response = await axios.get(`https://api.example.com/data/${selectedOptions.option1}/${selectedOptions.option2}/${selectedOptions.option3}/${selectedOptions.option4}/${selectedOptions.option5}`);
       // setResponseData(response.data);
       const response = await axios.get(
-        `https://prokerala-api-proxy.appilivamsikrishna.workers.dev/v2/astrology/chart?ayanamsa=1&coordinates=10.214747,78.097626&datetime=2022-03-17T10:50:40Z&chart_type=rasi&chart_style=south-indian&format=svg&la=en`,
-        {
-          headers: {
-            Authorization:
-              "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJiZjM5MmEwNC1mMThiLTRlMmEtOTg4OS02OGE1MjNjZjY3MDQiLCJqdGkiOiJkYjEzMWYzMzQzMTNhN2U4ZjU2NWZlMGQ4N2NlM2Y4NmUxMWM1ZmJjMGI3ZDRmZmRkNzNmNzVjZGZjZDJkYmI3MDliZTZiNzM0NzNhOWRhZSIsImlhdCI6MTcwODU4MTMwMi4wNzgxMDIsIm5iZiI6MTcwODU4MTMwMi4wNzgxMDMsImV4cCI6MTcwODU4NDkwMi4wNzc5ODYsInN1YiI6IjdmODdlOWQ4LTdmOGQtNDJhNC05ZGFmLTE1NGMzODE3ZjlkZCIsInNjb3BlcyI6W10sImNyZWRpdHNfcmVtYWluaW5nIjo1MDAwLCJyYXRlX2xpbWl0cyI6W3sicmF0ZSI6NSwiaW50ZXJ2YWwiOjYwfV19.fedylLkoRAqXChlM7fJXeu0LCf0WY9u6D6kS64ohfFofmASfFzRqMHSc43mSG87vhVWP10dumylVou8RSB3_804QPjWjiklSIJe1BnK6ezvLX6axUQ7eG_f_b32KfqK3xmqF2i7q8HTI4D8Pwu_9_ULJgkTmkXaXOXD7weoByU5oqj3-nNQxDfPNnbKPOAiGy2_cMJ9nUC_1agSNTVZATS-vmAP5NdyOTkpkl8NfUr1tUHUmX0S_i-z1JrpMdJV6pKRDVb3_O9yL-co3vTNMCzxK6c1KCA7X20g7YbcuV_L8Jj6tmznnbARx2v8JLqOXfif6MFKl8uTyXam9M6zHTw",
-          },
-        }
+        `https://prokerala-api-proxy.appilivamsikrishna.workers.dev/v2/astrology/chart?ayanamsa=1&coordinates=10.214747,78.097626&datetime=2022-03-17T10:50:40Z&chart_type=rasi&chart_style=south-indian&format=svg&la=en`
       );
+      setResponse(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+
+    setLoading(false);
+  };
+
+  const handleClic2 = async () => {
+    setLoading(true);
+    setError(null);
+    setResult(null);
+    try {
+      // const response = await axios.get(`https://api.example.com/data/${selectedOptions.option1}/${selectedOptions.option2}/${selectedOptions.option3}/${selectedOptions.option4}/${selectedOptions.option5}`);
+      // setResponseData(response.data);
+      const response = await axios.get(
+        `https://prokerala-api-proxy.appilivamsikrishna.workers.dev/v2/astrology/kundli?ayanamsa=1&coordinates=10.214747,78.097626&datetime=2022-03-17T10:50:40Z&la=en`
+      );
+      console.log(response.data);
       setResponse(response.data);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -100,13 +114,19 @@ const AstroForm = () => {
             label="Age"
             onChange={handleChange}
           >
-            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={10}>Tentyx</MenuItem>
             <MenuItem value={20}>Twenty</MenuItem>
             <MenuItem value={30}>Thirty</MenuItem>
           </Select>
         </FormControl>
 
         <Button variant="contained" onClick={handleClick} disabled={loading}>
+          {loading ? "Loading..." : "Submit"}
+        </Button>
+        {error && <div>Error: {error}</div>}
+        {result && <div>Result: {JSON.stringify(response)}</div>}
+
+        <Button variant="contained" onClick={handleClic2} disabled={loading}>
           {loading ? "Loading..." : "Submit"}
         </Button>
         {error && <div>Error: {error}</div>}
